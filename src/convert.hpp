64afa6bced024a7f1c6750016f102d7da8d0a45f
Michael Dawson-Haggerty <mikedh@kerfed.com>
2024-03-11 14:27:07 -0400

    add named arguments

--- src/convert.hpp
+++ src/convert.hpp
@@ -15,21 +15,9 @@ static const char *errorInvalidOutExtension =
 
 /// Transcode STEP to glTF
 static int step_to_glb(char *in, char *out, Standard_Real tol_linear,
-                       Standard_Real tol_angle) {
-  Standard_Boolean gltfIsBinary = Standard_False;
-  // glTF format depends on output file extension
-  const char *out_ext = strrchr(out, '.');
-  if (!out_ext) {
-    std::cerr << "Error: " << errorInvalidOutExtension << std::endl;
-    return 1;
-  } else if (strcasecmp(out_ext, ".gltf") == 0) {
-    gltfIsBinary = Standard_False;
-  } else if (strcasecmp(out_ext, ".glb") == 0) {
-    gltfIsBinary = Standard_True;
-  } else {
-    std::cerr << "Error: " << errorInvalidOutExtension << std::endl;
-    return 1;
-  }
+                       Standard_Real tol_angle, Standard_Boolean tol_relative,
+                       Standard_Boolean merge_primitives,
+                       Standard_Boolean use_parallel) {
 
   // Creating XCAF document
   Handle(TDocStd_Document) doc;
@@ -66,30 +54,36 @@ static int step_to_glb(char *in, char *out, Standard_Real tol_linear,
             << tol_angle << ") ..." << std::endl;
 
   XSControl_Reader reader = stepReader.Reader();
-
   for (int shape_id = 1; shape_id <= reader.NbShapes(); shape_id++) {
     TopoDS_Shape shape = reader.Shape(shape_id);
-
     if (shape.IsNull()) {
       continue;
     }
-
-    BRepMesh_IncrementalMesh Mesh(shape, tol_linear, Standard_False, tol_angle,
-                                  Standard_True);
+    // Mesh the shape.
+    BRepMesh_IncrementalMesh Mesh(shape, tol_linear, tol_relative, tol_angle,
+                                  use_parallel);
     Mesh.Perform();
   }
 
-  TColStd_IndexedDataMapOfStringString theFileInfo;
+  // Always export as binary GLB
+  // Otherwise this is a multi-file affair.
+  RWGltf_CafWriter cafWriter(out, Standard_True);
 
-  RWGltf_CafWriter cafWriter(out, gltfIsBinary);
-  // TODO : set parameters here like "merge faces into one primitive"
-  // and "use MAT4 transforms, always
+  // Set flag to merge faces within a single part.
+  // May reduce JSON size thanks to smaller number of primitive arrays.
+  cafWriter.SetMergeFaces(merge_primitives);
+
+  // Set multithreaded execution.
+  cafWriter.SetParallel(use_parallel);
+
+  // Always export as matrices rather than a decomposed
+  // rotation-translation-scale
+  cafWriter.SetTransformationFormat(RWGltf_WriterTrsfFormat_Mat4);
 
   Message_ProgressRange progress;
-
+  TColStd_IndexedDataMapOfStringString theFileInfo;
   if (!cafWriter.Perform(doc, theFileInfo, progress)) {
-    std::cerr << "Error: Failed to write " << (gltfIsBinary ? "binary " : "")
-              << "glTF to file !" << std::endl;
+    std::cerr << "Error: Failed to write glTF to file !" << std::endl;
     return 1;
   }
 
--- src/main.cpp
+++ src/main.cpp
@@ -15,9 +15,37 @@ PYBIND11_MODULE(cascadio, m) {
         A module for converting BREP files into GLB.
     )pbdoc";
 
-  m.def("step_to_glb", &step_to_glb, R"pbdoc(
-        Convert a step file to a GLB file.
-    )pbdoc");
+  m.def("step_to_glb",
+	&step_to_glb,
+R"pbdoc(
+Convert a step file to a GLB file.
+
+Parameters
+----------
+file_name
+  The input STEP file to load.
+file_out
+  The path to save the GLB file.
+tol_linear
+  How large should linear deflection be allowed.
+tol_angular
+  How large should angular deflection be allowed.
+tol_relative
+  Is tol_linear relative to edge length, or an absolute distance?
+merge_primitives
+  Produce a GLB with one mesh primitive per part.
+use_parallel
+  Use parallel execution to produce meshes and exports.
+
+)pbdoc",
+	py::arg("file_name"),
+	py::arg("file_out"),
+	py::arg("tol_linear") = 0.01,
+	py::arg("tol_angular") = 0.5,
+	py::arg("tol_relative") = false,
+	py::arg("merge_primitives") = true,
+	py::arg("use_parallel") = true
+	);
 
 #ifdef VERSION_INFO
   m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
